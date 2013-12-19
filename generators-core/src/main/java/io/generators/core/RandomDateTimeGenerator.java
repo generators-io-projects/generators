5c3ef33dd6137836e4f90a1278f0eb77dbb3db6d
tomas.klubal <tomas.klubal@generators.io>
2013-12-19 22:56:06 +0000

    Added support for generating random dates in specified range

--- generators-core/src/main/java/io/generators/core/RandomDateTimeGenerator.java
+++ generators-core/src/main/java/io/generators/core/RandomDateTimeGenerator.java
@@ -3,16 +3,52 @@ package io.generators.core;
 import org.joda.time.DateTime;
 
 import java.security.SecureRandom;
-import java.util.Random;
+
+import static com.google.common.base.Objects.firstNonNull;
+import static org.joda.time.DateTime.now;
 
 /**
  * Generates random {@link org.joda.time.DateTime}
  */
 public class RandomDateTimeGenerator implements Generator<DateTime> {
-    private Random random = new SecureRandom();
+    private static final int YEAR_BOUND = 100;
+    private final PositiveLongRandom random = new PositiveLongRandom();
+    private final Long from;
+    private final Long to;
+
+    public RandomDateTimeGenerator() {
+        this.from = null;
+        this.to = null;
+    }
+
+    public RandomDateTimeGenerator(DateTime from, DateTime to) {
+        this.from = from.getMillis();
+        this.to = to.getMillis();
+
+    }
 
     @Override
     public DateTime next() {
-        return DateTime.now().plusDays(random.nextInt(365 * 100) * (random.nextBoolean() ? 1 : -1)).plusMillis(random.nextInt());
+        DateTime now = now();
+        long lowerBound = firstNonNull(from, now.minusYears(YEAR_BOUND).getMillis());
+        long upperBound = firstNonNull(to, now.plusYears(YEAR_BOUND).getMillis());
+
+        long millis = random.nextLong(upperBound - lowerBound + 1) + lowerBound;
+        return new DateTime(millis);
+    }
+
+    /**
+     * Provides method to generate positive longs in range [0,n)
+     */
+    private class PositiveLongRandom extends SecureRandom {
+
+        public long nextLong(long n) {
+            long bits, val;
+            do {
+                bits = ((long) (next(31)) << 31) + next(32); //generating positive long + int
+                val = bits % n;
+            } while (bits - val + (n - 1) < 0);
+            return val;
+        }
     }
 }
--- generators-core/src/test/java/io/generators/core/RandomDateTimeGeneratorTest.java
+++ generators-core/src/test/java/io/generators/core/RandomDateTimeGeneratorTest.java
@@ -1,18 +1,19 @@
 package io.generators.core;
 
 import org.joda.time.DateTime;
+import org.joda.time.ReadableInstant;
 import org.junit.Test;
 
+import java.util.NavigableSet;
 import java.util.Set;
 
 import static com.google.common.collect.Sets.newHashSet;
+import static com.google.common.collect.Sets.newTreeSet;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.joda.time.DateTime.now;
 
-/**
- */
 public class RandomDateTimeGeneratorTest {
-
 
     @Test
     public void shouldGenerateRandomDateTime() {
@@ -26,5 +27,54 @@ public class RandomDateTimeGeneratorTest {
         }
 
         assertThat(dateTimeSet, hasSize(100));
+    }
+
+    @Test
+    public void shouldGenerateRandomDateTimeInTheSpecifiedRange() {
+        //Given
+        DateTime now = now();
+        DateTime from = now.minusYears(100);
+        DateTime to = now.plusYears(200);
+        Generator<DateTime> dateTimeGenerator = new RandomDateTimeGenerator(from, to);
+        NavigableSet<DateTime> dateTimeSet = newTreeSet();
+
+        //When
+        for (int i = 0; i < 1000; i++) {
+            DateTime next = dateTimeGenerator.next();
+            dateTimeSet.add(next);
+        }
+
+        //Then
+        assertThat(dateTimeSet, hasSize(1000));
+        assertThat(dateTimeSet.first(), greaterThanOrEqualTo((ReadableInstant) from));
+        assertThat(dateTimeSet.last(), lessThanOrEqualTo((ReadableInstant) to));
+    }
+
+    @Test
+    public void shouldGenerateRandomDateTimeInTheSpecifiedVerySmallRange() {
+        //Given
+        DateTime now = now();
+        DateTime from = now;
+        DateTime to = now.plus(1);
+        Generator<DateTime> dateTimeGenerator = new RandomDateTimeGenerator(from, to);
+        NavigableSet<DateTime> dateTimeSet = newTreeSet();
+
+        //When
+        UniqueGenerator<DateTime> dateTimeUniqueGenerator = new UniqueGenerator<>(dateTimeGenerator);
+        dateTimeSet.add(dateTimeUniqueGenerator.next());
+        dateTimeSet.add(dateTimeUniqueGenerator.next());
+
+        //Then
+        assertThat(dateTimeSet, hasItems(from, to));
+    }
+
+    @Test
+    public void shouldGenerateNowDateTime() {
+        //Given
+        DateTime now = now();
+        Generator<DateTime> dateTimeGenerator = new RandomDateTimeGenerator(now, now);
+
+        //When & Then
+        assertThat(dateTimeGenerator.next(), is(now));
     }
 }
