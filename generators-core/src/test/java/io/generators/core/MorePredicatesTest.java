ac68964f6e6aa402846a5c001deb52c440626ecf
David Bliss <github@davidbliss.me>
2013-12-26 13:01:04 +0000

    generators-19 Add test for MorePredicates

---
+++ generators-core/src/test/java/io/generators/core/MorePredicatesTest.java
@@ -0,0 +1,56 @@
+package io.generators.core;
+
+import com.google.common.base.Predicate;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import static io.generators.core.MorePredicatesTest.PredicateDataPoint.create;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(Theories.class)
+public class MorePredicatesTest {
+
+    private static final Predicate<Integer> IN_PREDICATE = MorePredicates.in(6, 7, 8);
+
+    @DataPoints
+    public static final PredicateDataPoint<Integer>[] DATA_POINTS = new PredicateDataPoint[]{
+            create(IN_PREDICATE, 5, false),
+            create(IN_PREDICATE, 6, true),
+            create(IN_PREDICATE, 7, true),
+            create(IN_PREDICATE, 8, true),
+            create(IN_PREDICATE, 9, false),
+    };
+
+    @Theory
+    public void shouldMatchPredicate(PredicateDataPoint input) {
+        // When
+        boolean result = input.apply();
+
+        // Then
+        assertThat(input.value + " is accepted by predicate", result, is(input.result));
+    }
+
+    static class PredicateDataPoint<T> {
+
+        final Predicate<T> predicate;
+        final T value;
+        final boolean result;
+
+        PredicateDataPoint(Predicate<T> predicate, T value, boolean result) {
+            this.predicate = predicate;
+            this.value = value;
+            this.result = result;
+        }
+
+        boolean apply() {
+            return predicate.apply(value);
+        }
+
+        static <T> PredicateDataPoint<T> create(Predicate<T> predicate, T value, boolean result) {
+            return new PredicateDataPoint<>(predicate, value, result);
+        }
+    }
+}
